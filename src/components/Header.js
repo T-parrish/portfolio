986ad15140aa7e2669acfc111ee3ca7f6788a930
Taylor Parrish <twpearish@gmail.com>
2018-05-22 20:17:40 +0800

    added styling, animations, and integrated typist library

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export const Header = () => (
 	<header className="header">
 		<div className="content-container">
 			<div className="nav__wrapper">
-				<div className="nav-box__one">
+				<div className="nav-box__one fade--top">
 					<h1 className="nav__title">Taylor Parrish</h1>
 					<div className="nav__links">
 						<NavLink to='/' activeClassName="is-active" exact={true}>Home </NavLink>
@@ -13,7 +13,7 @@ export const Header = () => (
 						<NavLink to='/contact' activeClassName="is-active">Contact </NavLink>
 					</div>
 				</div>
-				<div className="nav-box__two">
+				<div className="nav-box__two fade--in">
 					<a href="https://twitter.com/twpearish"><i className="fab fa-twitter"></i></a>
 					<a href="https://www.instagram.com/et.and.me"><i className="fab fa-instagram"></i></a>
 					<a href="www.linkedin.com/in/taylor-parrish-55663379"><i className="fab fa-linkedin-in"></i></a>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Header from './Header';
+import Typist from 'react-typist';
+import { Link } from 'react-router-dom'; 
 
 export const HomePage = () => (
     <div>
         <div className="home">
             <div className="home__content">
-                <h1 className="home__title">Hello, I'm Taylor</h1>
-                <button className="button_two"> Who? </button>
+                <h1 className="home__title">   
+                    <Typist>
+                        <Typist.Delay ms={2000} />
+                            <span>Hey! </span>
+                        <Typist.Delay ms={2500} />
+                            <span>My name is Taylor.</span>
+                        <Typist.Backspace count={24} delay={1000} />
+                        <Typist.Delay ms={500} />
+                            <span>Welcome to my Portfolio!</span> 
+                    </Typist>
+                </h1>
+                <Link to="/dashboard"><button className="button_two fade--button__main"> Who? </button></Link>
             </div>
         </div>
         <div className="wave-container">
